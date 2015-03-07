99f84f36c7bbb37ddea1f114f82ab575f06a149f
Andsbf <andsbf@gmail.com>
2015-03-07 20:03:47 +0000

    game v0.2 update, all created in classes

--- game.rb
+++ game.rb
@@ -1,45 +1,111 @@
-class Players
+class Player
   
   attr_accessor :name, :score, :lives
-  @@population =[]
+
   def initialize(name)  
     # Instance variables  
     @name = name  
     @lives = 3
     @score = 0
-    @@population << self
+    
     
   end  
   
-  def self.population
-    @@population
+  def display
+     
+    puts "\nPlayer:#{@name} => Lives: #{@lives}; Score: #{@score}\n"
+
+  end  
+
+  
+end 
+##############################################################################################################################
+
+class Game
+
+  @@gamers = []
+  @@active_players = []
+  @@current_player_index = 0
+  @@round_index = 0
+  @@num_players = 0
+
+  def initialize
   end
 
+  def create_player
+    p "Player #{@@gamers.length + 1} name:"  
+    player_name_input = gets.chomp
+    player_name_input = Player.new(player_name_input)
+    @@gamers << player_name_input
+    @@active_players  << player_name_input
+  end
 
-  def turn
+  def sets
 
-    num1 = rand(1..20)
-    num2 = rand (1..20)
+    while @@num_players == 0
+      p "How many people will play?"
+      @@num_players = Integer(gets) rescue nil
+      @@num_players = 0 if @@num_players.class != Fixnum 
+    end
+
+    while @@round_index == 0
+      p "Number of rounds to be played?"
+      @@round_index = Integer(gets)*2 rescue nil
+      @@round_index = 0 if @@round_index.class != Fixnum  
+      @@round_index = 0 unless @@round_index.even? 
+    end
+  end
+
+  def round
+
+    num1, num2 = rand(1..20) , rand(1..20)
     op = ['+','-','*','/'][rand(4)]
-    puts "\n#{@name}, What does #{num1} #{op} #{num2}  is equal?\n"
+    
+    puts "\n#{current_player.name}, What does #{num1} #{op} #{num2}  is equal?\n"
     right_answer = num1.method(op).(num2)  
 
     user_input = nil
     while user_input.nil?
       user_input = Integer(gets) rescue nil
     end
-    user_input == right_answer ? @score =+ 1 : @lives -= 1 
     
-  end  
-  
-  def display
-     
-    puts "\nPlayer:#{@name} ; Lives: #{@lives}; Score: #{@score}\n"
-
-  end  
-
-  def self.active
-    @@population.select{|p| p.lives > 0 }
+    result = user_input == right_answer
+    result ? current_player.score += 1 : current_player.lives -= 1 
+    result
   end
-end 
+
+  def update
+    @@round_index -= 1
+    @@current_player_index == (@@active_players.length - 1) ? @@current_player_index = 0 : @@current_player_index += 1 
+    @@active_players = @@gamers.select{|x| x.lives > 0}
+  end
+
+  def current_player
+    @@active_players[@@current_player_index]
+  end
+
+  def pick_winner
+    winner = @@active_players.sort_by{|x| [x.score, x.lives]}.reverse
+    [winner[0].score,winner[0].lives] == [winner[1].score,winner[1].lives] ? "It is a draw" : ( winner.first.name + " is the winner" ) 
+  end
+
+  def round_index
+    @@round_index
+  end
+
+  def gamers
+    @@gamers
+  end
+
+  def active_players
+    @@active_players
+  end
+
+  def num_players
+    @@num_players
+  end
+
+
+
+end
 
--- main.rb
+++ main.rb
@@ -1,41 +1,22 @@
-#main
+
 require './game.rb'
 require 'pry'
 
+game_1 = Game.new
+game_1.sets
+game_1.num_players.times {game_1.create_player}
 
+while ( game_1.active_players.length > 1 ) && ( game_1.round_index > 0 ) 
 
-p "Player 1 name:"  
-player_1_name = gets.chomp
-player_1 = Players.new(player_1_name)
+  result = game_1.round
+  game_1.update
+  game_1.gamers.each{ |g| g.display } unless result
 
-p "Player 2 name:"
-player_2_name = gets.chomp
-player_2 = Players.new(player_2_name)
-
-
-rounds = 0
-while rounds == 0
-  p "Number of rounds to be played?(Only even numbers accepted)"
-  rounds = Integer(gets) rescue nil
-  rounds = 0 unless rounds.even? 
-end
-
-# binding.pry
-
-
-
-1.upto(rounds) do |i|
-  puts "ROUND : #{i}"
-  
-  Players.population.length.times do |j|
-    player_b4_turn = Players.population[j].lives
-    Players.population[j].turn 
-    #binding.pry
-    break if Players.population[j].lives == 0
-    Players.population.each{|i| i.display} if player_b4_turn != Players.population[j].lives 
-  end
-  #binding.pry
-  break  if Players.active.length < 2 
 end
 puts "\nGAME FINAL SCORE\n"
-Players.population.each{|i| i.display}
+
+game_1.gamers.each{ |g| g.display } unless game_1.active_players.length < 1
+p game_1.pick_winner
+
+
+  
